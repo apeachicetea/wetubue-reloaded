62224be1124aa3cb49c7beb1cbd5087607c368f5
kimdaeyoon <eodbszla@gmail.com>
2022-01-21 10:49:48 +0900

    CRUD done

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@ import Video from "../models/Video.js";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("home", {pageTitle: "Home", videos});
   }
   catch {
@@ -65,3 +65,17 @@ export const postUpload = async (req, res) => {
 
 }
 
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  res.redirect('/');
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if(keyword) {
+    videos = await Video.find({ title: { $regex: new RegExp(keyword, "i") } })
+  }
+  res.render("search", { pageTitle:"Search", videos });
+};
