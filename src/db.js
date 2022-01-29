7e94b47a4ab9e5ac995b4e0e491adc11b30a37b2
kimdaeyoon <eodbszla@gmail.com>
2022-01-29 22:17:02 +0900

    Edit errorrrrrrr

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ mongoose.connect(process.env.DB_URL);
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("👍 Connected to DB");
-const handleError = () => console.log("DB Error", err);
+const handleError = (err) => console.log("DB Error", err);
 
 db.on("error", handleError);
 db.once("open", handleOpen);
